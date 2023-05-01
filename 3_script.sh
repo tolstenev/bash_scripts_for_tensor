eb57aca8bc170989b32cc9faa96ecb88a02e011d
Gleb Tolstenev <tolstenev.gleb@mail.ru>
2023-05-01 05:13:08 +0300

    Third script is done

--- 3_script.sh
+++ 3_script.sh
@@ -1,127 +1,108 @@
-# В директории N содержатся папки «продуктов» разных версий. Название папки продукта
-# состоит из латинских букв символа подчеркивания и версии. Формат версии следующий
-# «Х.У.*.*» т.е. версия – это числа разделенные точкой. Например, папка с именем
-# «Service_3.7.5.100» означает, что внутри находится дистрибутив продукта «Service» версии
-# 3.7.5.100. Таким образом, в директории N содержатся папки сервисов разных версий. Например:
-# N -|
-# Service_3.7.6.10
-# Service_3.7.6.100
-# Service_3.7.7
-# WebService_3.7.5.10
-# WebService_3.7.6
-# Что сделать:
-# Написать универсальный скрипт очистки директории N от папок продуктов версии старше\старее
-# заданной. У скрипта два параметра запуска: «директория N» «версия»
-# Доп. 3.7.5 старее, чем 3.7.5.10; 3.7.6.100 старее, чем 3.8
-
 #!/bin/bash
 
+# Version represent in A.B.C.D
+# FA - A in function
+# VA - A from input version
 
-function gen_dir() {
-    mkdir \
-"Service_3.7.7" \
-"sErvice_3.7.7" \
-"service_3.7.7" \
-"Service_3.7.6.10" \
-"Service_3.7.6.100" \
-"Service_3.7.68.100" \
-"Service_3.7.7" \
-"WebService_3.7.5.10" \
-"WebService_3.7.6" \
-"service_3.7" \
-"sErvice_3.7" \
-"Service_3.7" \
-"WebService___3.7.6" \
-"WebService___333.7.6" \
-"WebService_3.7.5.10a" \
-"abc_123.12" \
-"123" \
-"2abc" \
-"abc"
-}
-
-# gen_dir
-
-# rm relevant.txt dirs.txt
-
-# PATH_WHERE=$1
-# VERSION=$2
-# REGEX_DIRNAME="^\.\/[A-Za-z]*[_]{1}[0-9]{1}\.[0-9]{1}\.[0-9]*\.?[0-9]*$"
-
-VERSION_STOP=$1
-# FIRST_PART=${VERSION_STOP:0:1}
-# SECOND_PART=${VERSION_STOP:2:1}
-# NUM_STOP=$FIRST_PART$SECOND_PART
-
-str_of_dots="${VERSION_STOP//[^.]}"
-num_of_dots=${#str_of_dots}
-echo $num_of_dots
-
-
-for (( X=0; X < 10; X++ )); do
-    for (( Y=0; Y < 10; Y++ )); do
-        if [ $num_of_dots -eq 1 ]; then
-            if [ "$X$Y" -lt $NUM_STOP ]; then
-                echo "$X.$Y"
+remove_relevant_dirs() {
+    if [ $# -ge 3 ]; then
+        PATH_WHERE=$2
+        FA="[$3]"
+        if [ $1 = "recursive" ]; then
+            FB="[0-9]*"
+            FC="[0-9]*"
+            FD="[0-9]*"
+        elif [ $1 = "only" ]; then
+            FB="[-]*"
+            FC="[-]*"
+            FD="[-]*"
+        fi
+        
+        if [ $# -ge 4 ]; then
+            FB="[$4]"
+            if [ $# -ge 5 ]; then
+                FC="[$5]"
+                if [ $# -eq 6 ]; then
+                    FD="[$6]"
+                fi
             fi
         fi
+        
+        REGEX_DIRNAME="^\.\/[A-Za-z]*[_]{1}"$FA"\."$FB"\.?"$FC"\.?"$FD"$"
+        # echo $REGEX_DIRNAME
+        cd "$PATH_WHERE" && find . -type d >> $OLDPWD/dirs.txt
+        grep -E "$REGEX_DIRNAME" dirs.txt >> relevant.txt
+        # rm -rf actually
+        for each in $(cat relevant.txt); do
+            cp -r $each "${PATH_WHERE}_Trash/$each"
+        done
+        rm relevant.txt dirs.txt
+    fi
+}
 
-        if [ $num_of_dots -eq 2 ]; then
-            for (( Z=0; Z < 100; Z++ )); do
-                if [ "$X$Y$Z" -lt $NUM_STOP ]; then
-                    echo "$X.$Y.$Z"
+rm -rf _Trash
+mkdir _Trash
+
+if [ $# -eq 2 ]; then
+    PATH_WHERE="$1"
+    VERSION_STOP=$2
+    
+    PRE_IFS=$IFS
+    IFS='.'
+    read -ra arr <<< "$VERSION_STOP"
+    IFS=$PRE_IFS
+    
+    NUM_ABCD=${#arr[*]}
+    VA=${arr[0]}
+    VB=-1
+    VC=-1
+    VD=-1
+    if [ $NUM_ABCD -gt 1 ];then
+        VB=${arr[1]};
+        if [ $NUM_ABCD -gt 2 ];then
+            VC=${arr[2]};
+            if [ $NUM_ABCD -gt 3 ];then
+                VD=${arr[3]};
+            fi
+        fi
+    fi
+    
+    for (( A=0; A <= $VA; A++ )); do
+        if [ $A -lt $VA ]; then
+            remove_relevant_dirs \
+            "recursive" "$PATH_WHERE" "$A"
+            
+        elif [ $A -le $VA ]; then
+            for (( B=0; B <= $VB; B++ )); do
+                if [ $B -lt $VB ]; then
+                    remove_relevant_dirs \
+                    "recursive" "$PATH_WHERE" "$A" "$B"
+
+                elif [ $B -eq $VB ]; then
+                    if [ $VC -ne -1 ]; then
+                        remove_relevant_dirs \
+                        "only" "$PATH_WHERE" "$A" "$B"
+                    fi
+                    for (( C=0; C <= $VC; C++ )); do
+                        if [ $C -lt $VC ]; then
+                            remove_relevant_dirs \
+                            "recursive" "$PATH_WHERE" "$A" "$B" "$C"
+                        elif [ $C -eq $VC ]; then
+                            if [ $VD -ne -1 ]; then
+                                remove_relevant_dirs \
+                                "only" "$PATH_WHERE" "$A" "$B" "$C"
+                            fi
+                            for (( D=0; D <= $VD; D++ )); do
+                                if [ $D -lt $VD ]; then
+                                    remove_relevant_dirs \
+                                    "recursive" "$PATH_WHERE" \
+                                    "$A" "$B" "$C" "$D"
+                                fi
+                            done
+                        fi
+                    done
                 fi
             done
         fi
-
-        if [ $num_of_dots -eq 3 ]; then
-#            ...
-        fi
-
     done
-done
-
-# if [ $VERSION -eq "0.2" ]; then
-#     REGEX_DIRNAME="^\.\/[A-Za-z]*[_]{1}[0]{1}\.[1]{1}\.[0-9]*\.?[0-9]*$"
-# fi
-# if [ $VERSION -eq "0.3" ]; then
-#     REGEX_DIRNAME="^\.\/[A-Za-z]*[_]{1}[0]{1}\.[1]{1}\.[0-9]*\.?[0-9]*$"
-# fi
-
-
-
-
-
-
-
-
-
-# echo "path is $PATH_WHERE"
-# echo "Service_$VERSION"
-
-
-# cd $PATH_WHERE && find . -type d >> $OLDPWD/dirs.txt
-# grep -E $REGEX_DIRNAME dirs.txt >> relevant.txt
-
-
-
-
-# grep -E ^[A-Za-z]$ dirs.txt
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fi
